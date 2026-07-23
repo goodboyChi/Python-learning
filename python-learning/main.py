cd27449bfa408d817480e680fb70e9aca8728f90
@goodboy_chi <chibuzor.joseph638@gmail.com>
2026-07-23 23:02:43 +0100

    python learning

--- python-learning/main.py
+++ python-learning/main.py
@@ -1,5 +1,6 @@
 x =10
 x= 12
+is_true = True
 
 print("hello chibuzor!")
 def is_number():
@@ -7,5 +8,12 @@ def is_number():
 
 # print(x)
 print("hello chibuzor!")
-name = int(input("my name is:"))
-print(name + 90)
+# name = int(input("my name is:"))
+# print(name + 90)
+if x > 15 or is_true:
+ print("greater than 15")
+elif x >= 12 and is_true :
+ print("equal to 12")
+else:
+ print("less than 12")
+
