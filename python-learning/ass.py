89c147b1e2554ea7a1ab6d53e910b669edeeae83
chijoseph <chibuzorjoseph@gmail.com>
2026-07-27 16:38:46 +0100

    py

---
+++ python-learning/ass.py
@@ -0,0 +1,16 @@
+names = "abcdiesd"
+vowel= "aeiou"
+
+# print(names)
+
+count = 0
+for letter in names:
+    if letter in vowel:
+        count+=1
+    print(count)
+
+# names.append("python")
+
+# print(names[-1])
+
+# print(len(names))
