7889f06b66996abba0ddb2a18ce4b83810a24e48
chijoseph <chibuzorjoseph@gmail.com>
2026-07-29 16:35:24 +0100

    assignment

--- python-learning/assignment.py
+++ python-learning/assignment.py
@@ -11,13 +11,18 @@
 
 names = {
     "boy":54,
-    "girl":65,
+    45:65,
 }
-for key,val in enumerate(names):
+if "boy" in names:
+    print(names["boy"])
+else:
+    print("not found")
+for key,val in names.items():
     print(key,val)
-print(names["girl"])
+print(names[45])
 print(names.values())
-
+print(names.items())
+print(names.keys())
 # names.append("python")
 
 # print(names[-1])
